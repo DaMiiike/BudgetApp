62bd89fc2102ff8b2b381b55df0c1cee928eaffa
Michael Tariao <michael.tariao@medieinstitutet.se>
2021-11-28 22:33:51 +0100

    making it smarter with some JS

---
+++ app.js
@@ -0,0 +1,48 @@
+// INPUTS
+
+document.querySelector("button").addEventListener("click", inputBudget)
+
+
+//Lists
+let incomeList = [];
+let costList = [];
+
+
+function inputBudget(e) {
+    e.preventDefault();
+    
+    // Value
+    const select = document.querySelector("select")
+    const description = document.querySelector("#description").value;
+    const amount = document.querySelector("#amount").value;
+    
+    if(select.value=="+") {
+        console.log("här var det plus!!!!")  //to be removed at final stage
+
+        document
+        .querySelector(".income-container")
+        .innerHTML +=`<li class="income-list"> ${description} : ${amount}Kr</li>`;
+
+        incomeList.push(amount)
+
+        console.log(incomeList)
+
+    }
+    else if(select.value=="-"){
+        console.log("här var det minus!!! :(") // to be removed at the final stage
+
+        document
+        .querySelector(".cost-container")
+        .innerHTML += `<li class="cost-list"> ${description} : ${amount}Kr</li>`;
+
+        costList.push(amount)
+        console.log(costList);
+
+    }
+    else if(select.value=="choose"){
+        alert("Var vänlig och välj ett alternativ!")
+    }
+    
+}
+// console.log(amount.value);
+// console.log(description.value);
