5e0b7b01873d1df54a300a10004503a973b931d8
Michael Tariao <michael.tariao@medieinstitutet.se>
2021-11-30 21:56:48 +0100

    cleaning up some JS

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// INPUTS
+// INPUT TRIGGER
 
 document.querySelector("button").addEventListener("click", inputBudget)
-
 
 // Lists
 let incomeList = [];
 let costList = [];
 
-// // Totals
-// let incomeTot = 0;
-// let costTot = 0;
-
 function inputBudget(e) {
     e.preventDefault();
-
-
 
     // Value
     const select = document.querySelector("select")
@@ -22,51 +15,38 @@ function inputBudget(e) {
     const amount = document.querySelector("#amount").value;
     
     if(select.value=="+") {
-        console.log("här var det plus!!!!")  //to be removed at final stage
-
         document
         .querySelector(".income-container")
         .innerHTML +=`<li class="income-list"> ${description} : ${amount} Kr</li>`;
 
         incomeList.push(amount)
-
-        console.log(incomeList)
-
     }
     else if(select.value=="-"){
-        console.log("här var det minus!!! :(") // to be removed at the final stage
-
         document
         .querySelector(".cost-container")
         .innerHTML += `<li class="cost-list"> ${description} : ${amount} Kr</li>`;
 
         costList.push(amount)
-        console.log(costList);
-
     }
     else if(select.value=="choose"){
         alert("Var vänlig och välj ett alternativ!")
     }
     
-
+    // ADD INCOME
     let incomeSum = 0;
     for (var i = 0; i < incomeList.length; i++) {
         
         incomeSum += Number(incomeList[i]);
         
     }
-    console.log("Total intäkt " + incomeSum) // done, to be removed on final stage
-
+    //ADD COST
     let costSum = 0;
     for (var i = 0; i < costList.length; i++) {
 
         costSum += Number(costList[i]);
     }
-    console.log("Total kostnad " + costSum); // done, to be removed on final stage
-
+    
     let totSum = incomeSum - costSum;
-
-    console.log("hej" + totSum); // done, to be removed on final stage
 
     document
     .querySelector(".total-container")
